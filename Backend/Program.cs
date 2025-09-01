0e542b2bf08038a9400a33a2507ab42ee19239bb
Robin Brink <rbrink94@hotmail.com>
2025-09-01 15:11:53 +0200

    databas connectonstrings osv

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,23 +1,63 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder();
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var connection = String.Empty;
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddEnvironmentVariables().AddJsonFile("appsettings.Development.json");
+    connection = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+}
+else
+{
+    connection = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING");
+}
+
+builder.Services.AddDbContext<PersonDbContext>(options =>
+    options.UseSqlServer(connection));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
 }
 
-app.UseHttpsRedirection();
+app.MapGet("/", () => "Hello world!");
 
-app.UseAuthorization();
+app.MapGet("/Person", (PersonDbContext context) =>
+{
+    return context.Person.ToList();
+});
 
-app.MapControllers();
+app.MapPost("/Person", (Person person, PersonDbContext context) =>
+{
+    context.Add(person);
+    context.SaveChanges();
+});
 
 app.Run();
+
+public class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+public class PersonDbContext : DbContext
+{
+    public PersonDbContext(DbContextOptions<PersonDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Person> Person { get; set; }
+}
