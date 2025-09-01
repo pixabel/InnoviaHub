5907132f71869438a929bda6bdb7957b064129c0
Isabel Demirel <isabeldemirel@MacBook-Pro-2.local>
2025-09-01 09:44:39 +0200

    Added user model

---
+++ Backend/Models/User.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel;
+
+namespace InnoviaHub
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public required string PasswordHash { get; set; }
+        public bool IsAdmin { get; set; }
+
+    }
+}
